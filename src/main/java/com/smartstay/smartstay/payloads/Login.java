79c0bc02b8864a26dedc96c16f9360031fc6eae0
Tech Rules <66055840+techrules07@users.noreply.github.com>
2025-06-25 20:06:02 +0530

    controller advice added

--- src/main/java/com/smartstay/smartstay/payloads/Login.java
+++ src/main/java/com/smartstay/smartstay/payloads/Login.java
@@ -3,5 +3,5 @@ package com.smartstay.smartstay.payloads;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
-public record Login(@NotNull(message = "Email Id is required") String emailId, @NotNull(message = "Password is required") String password) {
+public record Login(@NotNull(message = "Email Id is required") @NotEmpty(message = "Email Id cannot be empty") String emailId, @NotNull(message = "Password is required") @NotEmpty(message = "Password cannot be empty") String password) {
 }
--- src/main/java/com/smartstay/smartstay/payloads/UpdateUserProfilePayloads.java
+++ src/main/java/com/smartstay/smartstay/payloads/UpdateUserProfilePayloads.java
@@ -1,7 +1,5 @@
 package com.smartstay.smartstay.payloads;
 
-import jakarta.persistence.Lob;
-
 public record UpdateUserProfilePayloads(String firstName, String lastName, String emailId, String mobile, String houseNo, String street, String landmark, String city, String state, int pincode) {
 
 }
