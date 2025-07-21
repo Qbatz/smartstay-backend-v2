60aebdcbac7814f51a11e867aa0c594051e7a808
Robin Isac <robin.isac@thesocialbytes.com>
2025-07-21 23:40:22 +0530

    Fix : Parent Id validation added for update role and removed isDeleted from update Role

--- src/main/java/com/smartstay/smartstay/payloads/roles/UpdateRoles.java
+++ src/main/java/com/smartstay/smartstay/payloads/roles/UpdateRoles.java
@@ -9,7 +9,6 @@ import java.util.List;
 public record UpdateRoles(
         String roleName,
         Boolean isActive,
-        Boolean isDeleted,
         @NotNull(message = "Permission list cannot be null")
         @Size(min = 1, max = 19, message = "Permission list must contain between 1 and 19 items")
         List<Permission> permissionList
