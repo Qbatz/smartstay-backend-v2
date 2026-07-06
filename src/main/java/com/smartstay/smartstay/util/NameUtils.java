a6fd731728ef079c939b2f43bf7a1b4406420f62
Antto <antto.gnanasiromani@cartulahealth.com>
2026-07-06 22:50:41 +0530

    I1710 : Tenant List - 500 Issue - GET

--- src/main/java/com/smartstay/smartstay/util/NameUtils.java
+++ src/main/java/com/smartstay/smartstay/util/NameUtils.java
@@ -18,25 +18,33 @@ public class NameUtils {
     public static String getInitials(String firstName, String lastName) {
         StringBuilder initials = new StringBuilder();
 
-        if (firstName != null) {
-            initials.append(firstName.toUpperCase().charAt(0));
+        boolean hasFirstName = firstName != null && !firstName.trim().isEmpty();
+        boolean hasLastName = lastName != null && !lastName.trim().isEmpty();
+
+        // Nothing to derive initials from (e.g. an incomplete draft customer).
+        if (!hasFirstName && !hasLastName) {
+            return "";
         }
-        if (lastName != null && !lastName.isEmpty()) {
-            initials.append(lastName.toUpperCase().charAt(0));
+
+        if (hasFirstName) {
+            initials.append(firstName.trim().toUpperCase().charAt(0));
+        }
+        if (hasLastName) {
+            initials.append(lastName.trim().toUpperCase().charAt(0));
         }
         else {
-            String[] fnameArr = firstName.split(" ");
+            // No last name: derive the second initial from the first name.
+            String fname = firstName.trim();
+            String[] fnameArr = fname.split(" ");
             if (fnameArr.length > 1) {
                 String lastString = fnameArr[fnameArr.length - 1];
                 initials.append(lastString.toUpperCase().charAt(0));
             }
+            else if (fname.length() > 1) {
+                initials.append(fname.toUpperCase().charAt(1));
+            }
             else {
-                if (firstName.length() > 1) {
-                    initials.append(firstName.toUpperCase().charAt(1));
-                }
-                else {
-                    initials.append(firstName.toUpperCase().charAt(0));
-                }
+                initials.append(fname.toUpperCase().charAt(0));
             }
         }
 
